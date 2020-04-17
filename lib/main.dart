4d1d94fcdac7af6c1e3935e4f9574d2555e6d69d
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-04-17 12:34:39 +0530

    BLoC Provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import './simple_bloc_delegate.dart';
+import './blocs/blocs.dart';
+import './services/services.dart';
+import './widgets/widgets.dart';
 
 
 void main() {
@@ -10,12 +13,51 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  
+  final WeatherAPIClient weatherAPIClient=WeatherAPIClient();
  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: BlocProvider(
+        create: (context) => WeatherBloc(weatherAPIClient: weatherAPIClient),
+        child: WeatherPage()
+      ),
+    );
+  }
+}
+
+
+class WeatherPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weathe App'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              final city = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CitySelection(),
+                ),
+              );
+              if (city != null) {
+                BlocProvider.of<WeatherBloc>(context)
+                    .add(FetchWeather(city: city));
+              }
+            },
+          )
+        ],
+      ),
+
+      body: Center(child: Text('jbk'),),
+      
+      
     );
   }
 }
